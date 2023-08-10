56330d2a961ec0d92de0e9b3e6e413eb9905b538
Mikhail <shaltynovm@gmail.com>
2023-08-10 16:08:54 +0530

    initial commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,20 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> finalList = new HashSet<>();
+        for (Integer i : sourceList) {
+            int a = i;
+            while (true) {
+                finalList.add(a);
+                if (a % 2 == 1) {
+                    a = a * 2;
+                    finalList.add(a);
+                    break;
+                } else {
+                    a = a / 2;
+                }
+            }
+        }
+        return finalList;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,17 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        HashSet<String> strings = new HashSet<>();
+        for (String string : firstSet) {
+            if (secondSet.contains(string) && !thirdSet.contains(string)) {
+                strings.add(string);
+            }
+        }
+        for (String s : thirdSet) {
+            if (!firstSet.contains(s) && !secondSet.contains(s)) {
+                strings.add(s);
+            }
+        }
+        return strings;
     }
 }
